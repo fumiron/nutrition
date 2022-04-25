627d374eb20fc3f25e32e2c3fcc0e5ba2c48632f
fumiron <haa13022@nuhw.ac.jp>
2022-04-25 16:25:01 +0900

    詳細一覧の表示

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -7,17 +7,20 @@ class FoodsController < ApplicationController
   end
 
   def create
-    @food = Food.create(food_params)
+    @food = Food.new(food_params)
     @nickname = current_user.nickname
     unless @food.save
       render :new
     end
   end
 
+  def show
+    @food = Food.find(params[:id])
+  end
   private
 
   def food_params
-    params.require(:food).permit(:name, :calorie, :protein, :fat, :saturated_fat, :n_6_fat, :n_3_fat, :cholesterol, :carbohydrate, :dietary_fiber, :vitamin_a, :vitamin_d, :vitamin_e, :vitamin_k, :vitamin_b1, :vitamin_b2, :vitamin_b6, :vitamin_b12, :vitamin_c, :niacin, :folate, :pantothenic, :natrium, :potassium, :calcium, :magnesium, :phosphorus, :iron, :zinc, :copper, :manganese, :iodine, :selenium, :chromium, :molybdenum).merge(user_id: current_user.id)
+    params.require(:food).permit(:name, :calorie, :protein, :fat, :saturated_fat, :n_6_fat, :n_3_fat, :cholesterol, :carbohydrate, :dietary_fiber, :vitamin_a, :vitamin_d, :vitamin_e, :vitamin_k, :vitamin_b1, :vitamin_b2, :vitamin_b6, :vitamin_b12, :vitamin_c, :niacin, :folate, :pantothenic, :biotin, :sodium, :potassium, :calcium, :magnesium, :phosphorus, :iron, :zinc, :copper, :manganese, :iodine, :selenium, :chromium, :molybdenum).merge(user_id: current_user.id)
   end
 
 end
