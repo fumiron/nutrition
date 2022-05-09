d49ea3b5c6f7ff0c267b31c6986121d84b157f93
fumiron <haa13022@nuhw.ac.jp>
2022-05-10 01:02:43 +0900

    CSS調整

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,4 +1,7 @@
 class FoodsController < ApplicationController
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+
   def index
     @food = Food.all
   end
@@ -18,15 +21,12 @@ class FoodsController < ApplicationController
   end
 
   def show
-    @food = Food.find(params[:id])
   end
 
   def edit
-    @food = Food.find(params[:id])
   end
 
   def update
-    @food = Food.find(params[:id])
     if @food.update(food_params)
       redirect_to food_path
     else
@@ -35,7 +35,6 @@ class FoodsController < ApplicationController
   end
 
   def destroy
-    @food = Food.find(params[:id])
     if @food.destroy
       redirect_to "/users/#{current_user.id}"
     end
@@ -51,6 +50,10 @@ class FoodsController < ApplicationController
 
   def food_params
     params.require(:food).permit(:name, :calorie, :protein, :fat, :saturated_fat, :n_6_fat, :n_3_fat, :cholesterol, :carbohydrate, :dietary_fiber, :vitamin_a, :vitamin_d, :vitamin_e, :vitamin_k, :vitamin_b1, :vitamin_b2, :vitamin_b6, :vitamin_b12, :vitamin_c, :niacin, :folate, :pantothenic, :biotin, :sodium, :potassium, :calcium, :magnesium, :phosphorus, :iron, :zinc, :copper, :manganese, :iodine, :selenium, :chromium, :molybdenum, :salt).merge(user_id: current_user.id)
+  end
+
+  def set_tweet
+    @food = Food.find(params[:id])
   end
 
 end
